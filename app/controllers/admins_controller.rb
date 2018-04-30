d04c16b5eb0518d273a4c1dc468e50d1c67cf477
Nathan Saunders <nathan_saunders@hotmail.co.uk>
2018-04-30 23:04:32 +0000

    Edited Gallery and admin area

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -2,6 +2,12 @@ class AdminsController < ApplicationController
    before_action :authenticate_admin!
    
    def dashboard
+     @homepage = Page.all.where("site_page = ?", "home").first
+      if !@homepage.blank?
+       return @homepage
+      else
+        return false
+      end
    end
     
   def index
